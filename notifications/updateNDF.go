fa453c9eeffe7c55118c0b5ce434492d29be565e
ksparakis <ksparakis@gmail.com>
2020-02-05 18:51:24 -0500

    add tests and copyright to all files

--- notifications/updateNDF.go
+++ notifications/updateNDF.go
@@ -1,3 +1,8 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
 package notifications
 
 import (
@@ -5,16 +10,21 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
-	"gitlab.com/elixxir/comms/mixmessages"
-	"gitlab.com/elixxir/comms/notificationBot"
+	"gitlab.com/elixxir/comms/connect"
+	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/primitives/id"
 	"gitlab.com/elixxir/primitives/ndf"
 )
 
 var noNDFErr = errors.New("Failed to get ndf from permissioning: rpc error: code = Unknown desc = Permissioning server does not have an ndf to give to Notification Bot")
 
+type pollCommInterface interface {
+	GetHost(hostId string) (*connect.Host, bool)
+	RequestNdf(host *connect.Host, message *pb.NDFHash) (*pb.NDF, error)
+}
+
 // PollNdf, attempts to connect to the permissioning server to retrieve the latest ndf for the notifications bot
-func PollNdf(currentDef *ndf.NetworkDefinition, comms *notificationBot.Comms) (*ndf.NetworkDefinition, error) {
+func PollNdf(currentDef *ndf.NetworkDefinition, comms pollCommInterface) (*ndf.NetworkDefinition, error) {
 	//Hash the notifications bot ndf for comparison with registration's ndf
 	hash := sha256.New()
 	ndfBytes := currentDef.Serialize()
@@ -22,7 +32,7 @@ func PollNdf(currentDef *ndf.NetworkDefinition, comms *notificationBot.Comms) (*
 	ndfHash := hash.Sum(nil)
 
 	//Put the hash in a message
-	msg := &mixmessages.NDFHash{Hash: ndfHash}
+	msg := &pb.NDFHash{Hash: ndfHash}
 
 	regHost, ok := comms.GetHost(id.PERMISSIONING)
 	if !ok {
