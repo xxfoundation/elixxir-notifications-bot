72ccad89233cdcabf3ab0fffc83dc06643eca241
jbhusson <jonah@elixxir.io>
2021-12-30 11:35:00 -0500

    Update buffer test

--- storage/buffer_test.go
+++ storage/buffer_test.go
@@ -16,6 +16,8 @@ func TestNotificationBuffer_Sorting(t *testing.T) {
 	uid2 := id.NewIdFromString("escaline", id.User, t)
 	eid2, _, _, _ := ephemeral.GetId(uid2, 16, time.Now().UnixNano())
 
+	eid1count := 0
+	eid2count := 0
 	for i := 0; i <= 5; i++ {
 		nd := []*notifications.Data{}
 		rand.Seed(time.Now().UnixNano())
@@ -31,8 +33,10 @@ func TestNotificationBuffer_Sorting(t *testing.T) {
 			var eid int64
 			if rid%2 == 0 {
 				eid = eid1.Int64()
+				eid1count++
 			} else {
 				eid = eid2.Int64()
+				eid2count++
 			}
 			nd = append(nd, &notifications.Data{
 				EphemeralID: eid,
@@ -47,6 +51,9 @@ func TestNotificationBuffer_Sorting(t *testing.T) {
 	sorted := nb.Swap()
 
 	if nl, ok := sorted[eid1.Int64()]; ok {
+		if len(nl) != eid1count {
+			t.Errorf("Did not find expected number of notifications for eid1.  Expected: %d, received: %d", eid1count, len(nl))
+		}
 		var last uint64
 		for _, n := range nl {
 			if n.RoundID < last {
@@ -56,6 +63,9 @@ func TestNotificationBuffer_Sorting(t *testing.T) {
 		}
 	}
 	if nl, ok := sorted[eid2.Int64()]; ok {
+		if len(nl) != eid2count {
+			t.Errorf("Did not find expected number of notifications for eid1.  Expected: %d, received: %d", eid2count, len(nl))
+		}
 		var last uint64
 		for _, n := range nl {
 			if n.RoundID < last {
