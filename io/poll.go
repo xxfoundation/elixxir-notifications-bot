80030a9deaae10a5cef0fd7930e79f3dc2c86019
Sydney Anne Erickson <sydney@elixxir.io>
2022-09-01 13:19:24 -0700

    Update copyright headers

--- io/poll.go
+++ io/poll.go
@@ -1,9 +1,9 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                          //
-//                                                                           //
-// Use of this source code is governed by a license that can be found in the //
-// LICENSE file                                                              //
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
 
 // Poll the network for the NDF. Users should create an ndf Poller with:
 //     poller := NewNdfPoller(Protocom object, permissioning host)
