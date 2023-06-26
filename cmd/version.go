fdeb2866055792a6a54187f545292d032f0a4cf7
jbhusson <jonah@elixxir.io>
2023-06-26 17:22:55 -0400

    Update version

--- cmd/version.go
+++ cmd/version.go
@@ -16,7 +16,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.2.0"
+const currentVersion = "2.2.1"
 
 func printVersion() {
 	fmt.Printf("Elixxir Notifications Server v%s -- %s\n\n", SEMVER, GITVERSION)
